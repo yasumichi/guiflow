506dbadaed71253d7e729222d090c09100c0bff8
Hiroki Daichi <hirokidaichi@gmail.com>
2016-04-08 18:24:43 +0900

    Add icon and some fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,70 @@
 var gulp = require('gulp');
 var zip = require("gulp-zip");
+var runseq = require("run-sequence");
 var packager = require("electron-packager");
 
 var electronVersion = "0.36.12";
 var del = require('del');
 gulp.task('clean', function(done) {
-    del(['package'], function() {
+    return del(['package'], function() {
         done();
     });
 });
 
-var packaging = ['win32', 'darwin', 'linux'].map(function(platform) {
-    var taskName = 'package:' + platform;
-    var distDir = 'dist';
-    gulp.task(taskName, function(done) {
-        packager({
-            dir: '.',
-            name: '',
-            arch: 'x64',
-            platform: platform,
-            out: 'package/' + platform,
-            version: electronVersion,
-            ignore: "/package($|/)",
-            asar: true,
-        }, function(err) {
-            done();
-        });
+gulp.task("package:win32", function(done) {
+    var platform = "win32";
+    return packager({
+        dir: '.',
+        name: '',
+        arch: 'x64',
+        platform: platform,
+        out: 'package/' + platform,
+        version: electronVersion,
+        ignore: "/package($|/)",
+        asar: true,
+    }, function(err) {
+        done();
     });
-    return taskName;
 });
-gulp.task('package', ['clean'].concat(packaging));
+
+gulp.task("package:linux", function(done) {
+    var platform = "linux";
+    return packager({
+        dir: '.',
+        name: '',
+        arch: 'x64',
+        platform: platform,
+        out: 'package/' + platform,
+        version: electronVersion,
+        ignore: "/package($|/)",
+        asar: true,
+    }, function(err) {
+        done();
+    });
+});
+
+
+gulp.task("package:darwin", function(done) {
+    var platform = "darwin";
+    return packager({
+        dir: '.',
+        name: '',
+        arch: 'x64',
+        platform: platform,
+        out: 'package/' + platform,
+        version: electronVersion,
+        ignore: "/package($|/)",
+        icon: "icon/gui_flow_icon.icons",
+        asar: true,
+    }, function(err) {
+        done();
+    });
+});
+gulp.task('package', function(cb) {
+    runseq(
+        'clean', [
+            'package:win32',
+            'package:darwin',
+            'package:linux'
+        ], cb);
+});
